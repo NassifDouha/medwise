7539bf62f3ec984d1843297f52cab5dd5a41b0ad
NassifDouha <nassif.douha@gmail.com>
2024-03-30 02:20:42 +0100

    partie forgot-pass et verification douha

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component,inject } from '@angular/core';
+import { Auth } from '@angular/fire/auth';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'medwise';
+}
+export class LoginComponent {
+  private auth: Auth = inject(Auth);
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { connectAuthEmulator, getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
+import { FormsModule } from '@angular/forms';
+import { ForgetPasswordComponent } from './component/forget-password/forget-password.component';
+import { VerifyEmailComponent } from './component/verify-email/verify-email.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ForgetPasswordComponent,
+    VerifyEmailComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"projet-medwise","appId":"1:336115863686:web:595fdcd9a01759825f8107","storageBucket":"projet-medwise.appspot.com","apiKey":"AIzaSyAUHQ0ekXMwjln-o37LQL8Vl7oxM5VLL5I","authDomain":"projet-medwise.firebaseapp.com","messagingSenderId":"336115863686"})),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    FormsModule
+  ],
+  
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+
